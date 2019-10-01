edd269297c531d5cafd79830d7b27756bafbdb03
Robin Klaus <rmklaus12@gmail.com>
2019-10-01 20:33:10 +1000

    submitted to exercism

--- exercism/ruby/two-fer/two_fer_test.rb
+++ exercism/ruby/two-fer/two_fer_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
+require 'minitest/pride'
 require_relative 'two_fer'
 
-# Common test data version: 1.1.0 c080bdf
 class TwoFerTest < Minitest::Test
   def test_no_name_given
     # skip
@@ -17,23 +17,6 @@ class TwoFerTest < Minitest::Test
     skip
     assert_equal "One for Bob, one for me.", TwoFer.two_fer("Bob")
   end
-
-  # Problems in exercism evolve over time, as we find better ways to ask
-  # questions.
-  # The version number refers to the version of the problem you solved,
-  # not your solution.
-  #
-  # Define a constant named VERSION inside of the top level BookKeeping
-  # module, which may be placed near the end of your file.
-  #
-  # In your file, it will look like this:
-  #
-  # module BookKeeping
-  #   VERSION = 1 # Where the version number matches the one in the test.
-  # end
-  #
-  # If you are curious, read more about constants on RubyDoc:
-  # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
     skip
