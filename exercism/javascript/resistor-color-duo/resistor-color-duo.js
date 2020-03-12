358c913093c0d26b6ac22405c8761db81c6e6cdc
Robin Klaus <rmklaus12@gmail.com>
2020-03-12 15:19:58 +1100

    passing

--- exercism/javascript/resistor-color-duo/resistor-color-duo.js
+++ exercism/javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,3 +1,16 @@
-export const decodedValue = () => {
+export const COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white"
+]
 
+export const decodedValue = (color) => {
+    return COLORS.indexOf(color[0]) * 10 + COLORS.indexOf(color[1]);
 }
